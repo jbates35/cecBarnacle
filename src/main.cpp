3c83bd7e1d78aa06222d5271bfaf5bc488dd2fee
jbates35 <jbates.productions@gmail.com>
2023-02-24 20:26:15 -0800

    first push

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,22 @@
 #include <Arduino.h>
-#define penis
+#define penis5 4
+
+int testSignal;
 
 void setup() {
   // put your setup code here, to run once:
-  int x = 0;
+  int x = 4;
+
+  testSignal = 4;
+
+  pinMode(5, OUTPUT);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  testSignal = analogRead(A4);
+  digitalWrite(5, testSignal);
+
 }
