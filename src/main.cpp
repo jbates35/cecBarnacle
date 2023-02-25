38381829853a08310cccda0a3e53e68609a5244d
Faran Bhatti <faran_bhatti@hotmail.com>
2023-02-25 00:09:01 -0500

    Commit it

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,10 @@
 #include "vexServo.h"
 #include "controller.h"
 #include "debounce_pin.h"
+#include <PS5BT.h>
+#include <usbhub.h>
+
+// Defines
 #define ENCODER_A 2
 #define ENCODER_B 3
 #define MOTOR_CW 9
@@ -9,7 +13,9 @@
 #define MOTOR_PWM 11
 #define POT_INPUT A0
 
+// reference this for PS4: https://github.com/felis/USB_Host_Shield_2.0/blob/master/examples/Bluetooth/PS4BT/PS4BT.ino
 vexServo thisServo(MOTOR_PWM);
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -22,6 +28,7 @@ void setup() {
 }
 
 void loop() {
+
 
   thisServo.run();
     // put your main code here, to run repeatedly:
