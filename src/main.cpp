7eed9e33a4dbf25523f7115dbdf9d3406d71a992
Faran Bhatti <faran_bhatti@hotmail.com>
2023-02-24 23:28:24 -0500

    first template

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,13 @@
 #include <Arduino.h>
-#define penis5 4
 
-int testSignal;
+
 
 void setup() {
   // put your setup code here, to run once:
-  int x = 4;
-
-  testSignal = 4;
-
-  pinMode(5, OUTPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-  testSignal = analogRead(A4);
-  digitalWrite(5, testSignal);
 
 }
